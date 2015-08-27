984a45c47504d9d722297e9078cfa5365d452271
Hassan Talat <hassan@Hassans-MacBook-Pro.local>
2015-08-26 19:18:20 -0700

    Initial commit

---
+++ app/src/main/java/com/example/hassan/funfacts/FunFactsActivity.java
@@ -0,0 +1,38 @@
+package com.example.hassan.funfacts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+
+public class FunFactsActivity extends AppCompatActivity {
+
+    private FactBook mfactBook = new FactBook();
+    private ColorWheel mColorWheel = new ColorWheel();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fun_facts);
+
+        final TextView factLabel = (TextView) findViewById(R.id.factTextView);
+        final Button showFactButton = (Button) findViewById(R.id.showFactButton);
+        final RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String fact = mfactBook.getFact();
+                int color= mColorWheel.getColor();
+                factLabel.setText(fact);
+                relativeLayout.setBackgroundColor(color);
+                showFactButton.setTextColor(color);
+            }
+        };
+        showFactButton.setOnClickListener(listener);
+    }
+
+
+}
